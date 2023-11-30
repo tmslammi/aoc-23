f967e5e741ae495d691c0f8b2d28ba0a5decd900
Lammi, Tuomas <tuomas.lammi_external@wartsila.com>
2023-11-30 23:10:36 +0200

    Initial commit

---
+++ day1/index.ts
@@ -0,0 +1,12 @@
+function p1() {
+    throw new Error("Not implemented")
+}
+
+function p2() {
+    throw new Error("Not implemented")
+}
+
+export default {
+    p1,
+    p2
+}
---
+++ util/index.ts
@@ -0,0 +1,9 @@
+export async function readFileText(
+    filename: string,
+) {
+    const file = Bun.file(filename)
+
+    const content = await file.text()
+
+    const lines = content.split("\n")
+}
